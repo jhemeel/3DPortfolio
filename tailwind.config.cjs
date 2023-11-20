b8584e11b4e41bb60612afe58da11fa8d9c2c376
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-11-20 22:55:16 +0100

    merging

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
+  content: ["./src/**/*.{js,jsx}"],
   // content: ["./src/**/*.{js,jsx}"],
-  // content: ["./src/**/*.{js,jsx}"],
-  content: ["./components/**/*.{js,ts,jsx,tsx}"],
+  // content: ["./components/**/*.{js,ts,jsx,tsx}"],
 
   mode: "jit",
   theme: {
